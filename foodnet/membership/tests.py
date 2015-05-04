04187014bf1729c50c70db37def1564915d1bd57
Mikkel Munch Mortensen <3xm@detfalskested.dk>
2015-05-04 20:45:41 +0200

    A couple of tests related to the UserProfile.privacy problem we had.

--- foodnet/membership/tests.py
+++ foodnet/membership/tests.py
@@ -1,11 +1,23 @@
 # -*- coding: utf-8 -*-
 from django.test import TestCase
-
 from django.core.urlresolvers import reverse
+from django.contrib.auth.models import User
 
 
 class TestMembership(TestCase):
+    def setUp(self):
+        self.user = User.objects.create(
+            username='testuser',
+            email='test@food.net'
+        )
+        self.user.save()
 
     def test_profile(self):
         resp = self.client.get(reverse('profile'))
         self.assertContains(resp, 'profile', 2, 200)
+
+    def test_user_profile(self):
+        self.assertIsNotNone(self.user.userprofile)
+
+    def test_user_profile_privacy(self):
+        self.assertFalse(self.user.userprofile.privacy)
