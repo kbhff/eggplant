4deea68903f751802e7f1ebb96dc784a9a9c1161
Benjamin Bach <benjaoming@gmail.com>
2015-06-15 12:37:54 +0200

    add check to see if file exists before overwriting

--- manage.py
+++ manage.py
@@ -7,13 +7,17 @@ try:
 except ImportError:
     
     from foodnet_project import settings
-    local_location = os.path.dirname(settings.__file__)
-    open(
-        os.path.join(local_location, 'local.py'),
-        "w"
-    ).write(
-        open(os.path.join(local_location, 'local.py.sample')).read()
+    local_location = os.path.join(
+        os.path.dirname(settings.__file__),
+        'local.py'
     )
+    if not os.path.isfile(local_location):
+        open(
+            local_location,
+            "w"
+        ).write(
+            open(os.path.join(local_location, 'local.py.sample')).read()
+        )
 
 if __name__ == '__main__':
     os.environ.setdefault('DJANGO_SETTINGS_MODULE',
