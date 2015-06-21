c9ccb89fe507cefd8d3c56c2001cbb29286de032
Francesco Zanitti <fzanitti@gmail.com>
2015-06-21 16:41:15 +0200

    added departmentadministrator to admin interface

--- foodnet/membership/admin.py
+++ foodnet/membership/admin.py
@@ -2,12 +2,13 @@ from django.contrib import admin
 
 
 from .models import UserProfile, Department, DepartmentInvitation, Account,\
-    AccountCategory, AccountMembership
+    AccountCategory, AccountMembership, DepartmentAdministrator
 
 
 admin.site.register(UserProfile)
 admin.site.register(Department)
 admin.site.register(DepartmentInvitation)
+admin.site.register(DepartmentAdministrator)
 admin.site.register(Account)
 admin.site.register(AccountCategory)
 admin.site.register(AccountMembership)
