82ce371930bd81c2e3e6d12e09a1d734a69b6897
Víðir Valberg Guðmundsson <valberg@orn.li>
2015-06-23 21:21:07 +0200

    Fixing small errors that slipped.

--- eggplant/membership/models/invitation.py
+++ eggplant/membership/models/invitation.py
@@ -5,7 +5,7 @@ from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-from ..common.utils import absolute_url_reverse
+from ...common.utils import absolute_url_reverse
 
 
 class InvitationBase(models.Model):
