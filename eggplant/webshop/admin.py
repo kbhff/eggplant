c15b6084536daf3fdf2d9c69b836fc7b7a80a2eb
Benjamin Bach <benjaoming@gmail.com>
2015-09-19 20:39:37 +0200

    admin for tax and category

--- eggplant/webshop/admin.py
+++ eggplant/webshop/admin.py
@@ -2,8 +2,14 @@ from django.conf import settings
 from django.contrib import admin
 
 
-from .models.inventory import Product
+from .models.inventory import Product, ProductCategory, ProductTax
 from .models.cart import Basket
+
+
 admin.site.register(Product)
+admin.site.register(ProductCategory)
+admin.site.register(ProductTax)
+
+
 if settings.DEBUG:
     admin.site.register(Basket)
