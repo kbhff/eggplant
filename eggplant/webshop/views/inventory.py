6973fb60a7615be281c2a365aebb603e4574c4ad
Benjamin Bach <benjaoming@gmail.com>
2015-09-19 13:20:52 +0200

    remove unused imports

--- eggplant/webshop/views/inventory.py
+++ eggplant/webshop/views/inventory.py
@@ -1,17 +1,9 @@
 
 import logging
 
-from django.contrib import messages
-from django.utils.translation import ugettext as _
-from django.views.generic.detail import DetailView
-from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import get_object_or_404, redirect, render
-from django.contrib.auth.decorators import user_passes_test
+from django.shortcuts import render
 
-from getpaid.forms import PaymentMethodForm
-
-from eggplant.membership.utils import is_active_account_owner
 from eggplant.webshop.models.inventory import Product
 from eggplant.webshop.models.cart import Basket
 
