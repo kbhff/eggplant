2d2ead15dc97fe9bf255fc3117856a77a20924a0
Benjamin Bach <benjaoming@gmail.com>
2015-10-03 13:25:52 +0200

    Remove som warnings, workaround for a PyDev error marker

--- eggplant/dashboard/models.py
+++ eggplant/dashboard/models.py
@@ -1,4 +1,1 @@
-# -*- coding: utf-8 -*-
-from django.db import models
-
-# Create your models here.
+# This module has to be there for the app to be a valid Django app
--- eggplant/market/urls.py
+++ eggplant/market/urls.py
@@ -1,44 +1,44 @@
 
 from django.conf.urls import url, include
 
-from .views import cart as cart_views
-from .views import inventory as inventory_views
-from .views import payment as payment_views
+from .views import cart
+from .views import inventory
+from .views import payment
 
 
 uuid_re_param = '[a-zA-Z0-9]{8}\-?[a-zA-Z0-9]{4}\-?[a-zA-Z0-9]{4}\-?' + \
     '[a-zA-Z0-9]{4}\-?[a-zA-Z0-9]{12}'
 
 payment_patterns = [
-    url(r'payment-list/$', payment_views.payment_list, name="payment_list"),
+    url(r'payment-list/$', payment.payment_list, name="payment_list"),
 
     url(r'payment-detail/(?P<pk>\d+)/$',
-        payment_views.payment_detail,
+        payment.payment_detail,
         name="payment_detail"),
 
     url(r'payment-info/(?P<pk>\d+)/$',
-        payment_views.payment_info,
+        payment.payment_info,
         name="order_info"),
 
     url(r'payment-accepted/(?P<pk>\d+)/$',
-        payment_views.payment_accepted,
+        payment.payment_accepted,
         name="payment_accepted"),
 
     url(r'payment-rejected/(?P<pk>\d+)/$',
-        payment_views.payment_rejected,
+        payment.payment_rejected,
         name="payment_rejected"),
-    url(r'$', payment_views.payments_home, name="payments_home"),
+    url(r'$', payment.payments_home, name="payments_home"),
 ]
 
 
 urlpatterns = [
     url(r'payment/', include(payment_patterns)),
-    url(r'add-to-cart/$', cart_views.add_to_cart, name="add_to_cart"),
-    url(r'remove-from-cart/$', cart_views.remove_from_cart,
+    url(r'add-to-cart/$', cart.add_to_cart, name="add_to_cart"),
+    url(r'remove-from-cart/$', cart.remove_from_cart,
         name="remove_from_cart"),
     url(r'your-cart/$',
-        cart_views.cart_details, name="cart_details"),
+        cart.cart_details, name="cart_details"),
     url(r'checkout/$',
-        cart_views.checkout, name="checkout"),
-    url(r'$', inventory_views.webshop_home, name="webshop_home"),
+        cart.checkout, name="checkout"),
+    url(r'$', inventory.webshop_home, name="webshop_home"),
 ]
