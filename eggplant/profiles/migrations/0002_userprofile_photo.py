912bd88e3989f105b7f73a86fe37418d9cc9cbbd
James Brooking <jambroo@gmail.com>
2015-11-07 13:11:14 +0100

    Adding photo field to profile model.
    This is stored as binary in the database. Storing it on the webserver's filesystem is not possible
    due to the use of Heroku. Down the track there should be steps taken to move these profile pictures
    to some service like Amazon S3 or Google Cloud.

---
+++ eggplant/profiles/migrations/0002_userprofile_photo.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('profiles', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='userprofile',
+            name='photo',
+            field=models.BinaryField(null=True),
+        ),
+    ]
