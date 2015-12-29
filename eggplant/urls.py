f0f6a5bbd3897fd7b5bcf4a309413849a7de6caa
Kræn Hansen (Socialsquare) <kraen@socialsquare.dk>
2015-12-29 23:27:27 +0100

    Fixing the two E124s

--- eggplant/urls.py
+++ eggplant/urls.py
@@ -2,14 +2,15 @@ from django.conf.urls import include, url
 
 urlpatterns = [
     url(r'^invitations/',
-        include('eggplant.invitations.urls', namespace='invitations')
-    ),
+        include('eggplant.invitations.urls', namespace='invitations')),
     url(r'^profiles/',
         include('eggplant.profiles.urls', namespace='profiles')),
     url(r'^departments/',
-        include('eggplant.departments.urls', namespace='departments',)
-    ),
-    url(r'^accounts/', include('eggplant.accounts.urls', namespace='accounts')),
-    url(r'^market/', include('eggplant.market.urls', namespace='market')),
-    url(r'^', include('eggplant.dashboard.urls', namespace='dashboard')),
+        include('eggplant.departments.urls', namespace='departments',)),
+    url(r'^accounts/',
+        include('eggplant.accounts.urls', namespace='accounts')),
+    url(r'^market/',
+        include('eggplant.market.urls', namespace='market')),
+    url(r'^',
+        include('eggplant.dashboard.urls', namespace='dashboard')),
 ]
