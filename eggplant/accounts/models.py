e50a3d89e829cae7367fb821e4bed7ac318dfe92
Kræn Hansen (Socialsquare) <kraen@socialsquare.dk>
2016-01-16 18:04:03 +0100

    Added a name an account

--- eggplant/accounts/models.py
+++ eggplant/accounts/models.py
@@ -1,7 +1,10 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
 
 class Account(models.Model):
+    name = models.CharField(max_length=50, blank=True)
+
     category = models.ForeignKey(
         'accounts.AccountCategory',
         related_name='accounts',
@@ -27,6 +30,16 @@ class Account(models.Model):
         'profiles.UserProfile',
         related_name='accounts'
     )
+
+    def name_or_profile_names(self):
+        if self.name != '':
+            return self.name
+        else:
+            # The accounts name would be the name of the profiles associated
+            # with it.
+            names = [p.user.first_name for p in self.user_profiles.all()]
+            names = ' & '.join(names)
+            return "%s's %s" % (names, _('account'))
 
     def __str__(self):
         is_active = 'active' if self.active else 'inactive'
