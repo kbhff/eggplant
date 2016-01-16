10f0613a39a86c07ef801d4299f4554347a8e7aa
Kræn Hansen (Socialsquare) <kraen@socialsquare.dk>
2016-01-16 20:32:10 +0100

    Added the initial migration for the roles app.

---
+++ eggplant/roles/migrations/0001_initial.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='RoleAssignment',
+            fields=[
+                ('id', models.AutoField(auto_created=True, verbose_name='ID', primary_key=True, serialize=False)),
+            ],
+        ),
+    ]
