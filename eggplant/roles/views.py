b794aece735d6d5cf778d9742ba5abb03535ef96
Kræn Hansen (Socialsquare) <kraen@socialsquare.dk>
2016-01-17 16:24:14 +0100

    Now using the right templates in the role views.

--- eggplant/roles/views.py
+++ eggplant/roles/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from eggplant.market.models import Product
+
 
 def role(request, role):
     if role == 'purchaser':
@@ -14,20 +16,29 @@ def role(request, role):
 
 
 def purchaser(request):
-    return render(request, 'eggplant/roles/purchaser.html')
+    show_disabled_products = 'show-disabled-products' in request.GET
+    if show_disabled_products:
+        products = Product.objects.all()
+    else:
+        products = Product.objects.filter(enabled=True)
+    ctx = {
+        'products': products,
+        'show_disabled_products': show_disabled_products
+    }
+    return render(request, 'eggplant/roles/purchaser/dashboard.html', ctx)
 
 
 def communicator(request):
-    return render(request, 'eggplant/roles/communicator.html')
+    return render(request, 'eggplant/roles/communicator/dashboard.html')
 
 
 def packer(request):
-    return render(request, 'eggplant/roles/packer.html')
+    return render(request, 'eggplant/roles/packer/dashboard.html')
 
 
 def cashier(request):
-    return render(request, 'eggplant/roles/cashier.html')
+    return render(request, 'eggplant/roles/cashier/dashboard.html')
 
 
 def accountant(request):
-    return render(request, 'eggplant/roles/accountant.html')
+    return render(request, 'eggplant/roles/accountant/dashboard.html')
