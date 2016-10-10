ecce7b8c94ab7e8c66355fc5acb96c6858c054c0
Benjamin Bach <benjaoming@gmail.com>
2016-10-10 23:58:57 +0200

    i18n support, gettext messages + url patterns

--- eggplant_project/settings/base.py
+++ eggplant_project/settings/base.py
@@ -90,6 +90,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
@@ -137,7 +138,7 @@ WSGI_APPLICATION = 'eggplant_project.wsgi.application'
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'en'
 
 LANGUAGES = (
     ('en', _('English')),
@@ -151,6 +152,8 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+
+PROJECT_PATHS = os.path.join(BASE_DIR, 'eggplant_project', 'locale')
 
 
 # Static files (CSS, JavaScript, Images)
--- eggplant_project/urls.py
+++ eggplant_project/urls.py
@@ -2,43 +2,44 @@
 from django.conf import settings
 
 from django.conf.urls import include, url
-from django.conf.urls.static import static
 from django.contrib import admin
 from django.views.generic.base import RedirectView
 from django.contrib.auth.views import logout
 
 import eggplant.profiles.views
-from eggplant.dashboard import views as dashboard_views
+from django.conf.urls.i18n import i18n_patterns
+
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-
     # allauth signup is disabled - we have our own signup flow
     url(r'^account/signup/$',
         RedirectView.as_view(url=settings.SIGNUP_URL_NAME, permanent=True),
         name='account_signup'),
-
     # override django-allauth password set and change views
     url(r'^account/password/set/$',
         eggplant.profiles.views.NewUserPassword.as_view(),
         name='account_set_password'),
-
     # override django-allauth logout so no confirmation is needed
     # see: http://www.sarahhagstrom.com/2013/09/the-missing-django-allauth-tutorial/#Remove_the_logout-confirmation_step
     url(r'^account/logout/$',
         logout,
         {'next_page': '/'}),
-
     url(r'^getpaid/', include('getpaid.urls')),
-
     url(r'^account/', include('allauth.urls')),
-
     url(r'^', include('eggplant.urls', namespace='eggplant')),
+]
 
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
+urlpatterns = i18n_patterns(*urlpatterns)
 
 if settings.DEBUG:
+    from django.conf.urls.static import static
+    from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+    # Serve static and media files from development server
+    urlpatterns += staticfiles_urlpatterns()
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
     import debug_toolbar
     urlpatterns += [
         url(r'^__debug__/', include(debug_toolbar.urls)),
